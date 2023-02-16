a2b5658ffe0283cf664383b090eac025ad6e8b04
GuCMartins <102696637+GuCMartins@users.noreply.github.com>
2023-02-16 19:34:23 -0300

    botoes sendo adicionados

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
-import { Head } from 'next/document'
+import { Button, HStack, Stack, Text, Wrap, WrapItem } from '@chakra-ui/react'
 
-export function Header() {
+export function Navbar() {
     return (
-        <Head>
-            <title>Fight on</title>
-        </Head>
+        <HStack
+            as="nav"
+            bg='black'
+        >
+            <Stack>
+                <Text>
+                    Colocar a logo+circulo
+                </Text>
+
+            </Stack>
+
+            <Stack direction='column'>
+                <Wrap spacing={4}>
+                    <WrapItem>
+                        <Button colorScheme='red'>Home</Button>
+                    </WrapItem>
+                    <WrapItem>
+                        <Button colorScheme='red'>Contato</Button>
+                    </WrapItem>
+                    <WrapItem>
+                        <Button colorScheme='red'>Funcionarios</Button>
+                    </WrapItem>
+                    <WrapItem>
+                        <Button colorScheme='red'>Login</Button>
+                    </WrapItem>
+                </Wrap>
+            </Stack>
+        </HStack>
     )
 }
